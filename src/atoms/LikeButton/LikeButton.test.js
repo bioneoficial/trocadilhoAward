ea2dcd621cc8dbfd615ee782a979ea1652e369e4
Joao Bione <joaph@ciandt.com>
2023-02-15 10:55:25 -0300

    refactor: update tests

--- src/atoms/LikeButton/LikeButton.test.js
+++ src/atoms/LikeButton/LikeButton.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import LikeButton from "./LikeButton";
 
 describe("<LikeButton />", () => {
@@ -8,5 +8,23 @@ describe("<LikeButton />", () => {
     const likeButton = screen.getByRole("button", { name: /like/i });
 
     expect(likeButton).toBeInTheDocument();
+  });
+
+  it("calls the onClick function when clicked", () => {
+    const onClick = jest.fn();
+    render(<LikeButton onClick={onClick} />);
+
+    const likeButton = screen.getByRole("button", { name: /like/i });
+    fireEvent.click(likeButton);
+
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it("displays the correct text content", () => {
+    render(<LikeButton />);
+
+    const likeButton = screen.getByRole("button", { name: /like/i });
+
+    expect(likeButton).toHaveTextContent("DEIXE SEU LIKE!");
   });
 });
